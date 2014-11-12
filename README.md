e70cbc205cc8a0a374c7884e30d00aa2d90a9cef
Antouhou <antouhou@gmail.com>
2014-11-12 13:24:50 +0400

    Added y rotation for camera, added 'canFly' option

--- README.md
+++ README.md
@@ -10,16 +10,18 @@ add this script to your html after three.js, and then:
 
 `var camera = new FirstPersonCamera();`
 
-and then in your scene render function: 
+and then add to your scene render function: 
 
-`camera.update();`
-`renderer.render(scene,camera.object);`
+`camera.update();
+ renderer.render(scene,camera.object);`
 
 ###Options
 
 movementSpeed - movement speed. Defaults to 1.
-
-target - three.js Vector3 for camera direction. Do not use it, until you don't understand how it works.
+rotationSpeed - rotation speed. Defaults to 0.02.
+canFly - allow camera change it y position by moving forward/backward. Defaults to false.
+defaultPosition - default position for camera. Defaults to {x: 0, y:10, z: 0}
+targetObj = a THREE.Object3D to place right before camera. It can be a sight, for example. Defaults to empty Object3D. 
 
 ###Controls
 
--- firstPersonCamera.js
+++ firstPersonCamera.js
@@ -1,24 +1,38 @@
 var FirstPersonCamera = function () {
 
+  var deltaX = 0;
+  var deltaZ = 0;
+  var deltaY = 0;
+  var lastPositionY = 0;
+  var lastPositionX = 0;
+  var lastPositionZ = 0;
+
   //Some options
   this.movementSpeed = 1;
   this.rotationSpeed = 0.02;
-  //this.canFly = false;
+  //Default camera position
+  this.defaultPosition = {x: 0, y:10, z: 0};
+  this.position = new THREE.Vector3(0,10,0);
+  //Enable this to allow camera change it y position by moving forward/backward
+  this.canFly = false;
+  //Creating target for the camera. You can redefine targetObj, so camera will follow any object,
+  //For example:
+  //var camera = new FirstPersonCamera();
+  //var someRedCube = new THREE.Mesh(someCubeGeometry, someCubeMaterial);
+  //camera.targetObj = someRedCube;
+  //and camera always will be focused on someRedCube.
+  this.targetObj = new THREE.Object3D();
 
   this.object = new THREE.PerspectiveCamera(45 , window.innerWidth / window.innerHeight , 0.1, 1000);
-  this.object.position.set(0,10,0);
+  this.object.position.set(this.defaultPosition.x, this.defaultPosition.y, this.defaultPosition.z);
 
-  //For debugging purposes
-  //var cubeGeometry = new THREE.CubeGeometry(4,4,4);
-  //var cubeMaterial = new THREE.MeshPhongMaterial(
-  //  {color: 0xff0000,specular: 0xffeeee,shininess: 100});
+  this.target = this.targetObj.position;
   //this.target = new THREE.Vector3(10,10,10);
-  //this.target = new THREE.Mesh(cubeGeometry,cubeMaterial);
-  //this.target = new THREE.Object3D();
-  //this.target.position.set(10,10,10);
-  this.target = new THREE.Vector3(10,10,10);
 
-  this.rotation = 0;
+  this.sideRotation = 0;
+  this.upRotation = 0;
+  this.deltaRotationX = 0;
+  this.deltaRotationY = 0;
 
   this.move = false;
   this.moveForward = false;
@@ -26,14 +40,10 @@ var FirstPersonCamera = function () {
   this.moveLeft = false;
   this.moveRight = false;
 
-  this.deltaX = 0;
-  this.deltaZ = 0;
-  this.lastPositionX = 0;
-  this.lastPositionZ = 0;
-
   this.update = function () {
-    this.lastPositionX = this.object.position.x;
-    this.lastPositionZ = this.object.position.z;
+    lastPositionX = this.object.position.x;
+    lastPositionZ = this.object.position.z;
+    lastPositionY = this.object.position.y;
 
     //Перемещаем камеру в соответсвии с нажатыми клавишами
     if (this.moveForward && !this.moveBackward) {
@@ -55,25 +65,41 @@ var FirstPersonCamera = function () {
 
     //Если камера переместилась, перемещаем и её цель
     if (this.move) {
+      if (!this.canFly) {
+        this.object.position.y = this.position.y;
+      }
       this.move = false;
-      this.deltaX = this.object.position.x - this.lastPositionX;
-      this.deltaZ = this.object.position.z - this.lastPositionZ;
-      this.target.x += this.deltaX;
-      this.target.z += this.deltaZ;
+      deltaX = this.object.position.x - lastPositionX;
+      deltaZ = this.object.position.z - lastPositionZ;
+      deltaY = this.object.position.y - lastPositionY;
+      this.target.x += deltaX;
+      this.target.z += deltaZ;
+      this.target.y += deltaY;
     }
 
     if (this.rotateLeft) {
       this.rotateLeft = false;
-      this.rotation -= this.deltaRotationX*this.rotationSpeed;
-      this.target.x = this.object.position.x+20*Math.cos(this.rotation);
-      this.target.z = this.object.position.z+20*Math.sin(this.rotation);
+      this.sideRotation -= this.deltaRotationX*this.rotationSpeed;
+      this.target.x = this.object.position.x+20*Math.cos(this.sideRotation);
+      this.target.z = this.object.position.z+20*Math.sin(this.sideRotation);
     }
 
     if (this.rotateRight) {
       this.rotateRight = false;
-      this.rotation += this.deltaRotationX*0.02;
-      this.target.x = this.object.position.x+20*Math.cos(this.rotation);
-      this.target.z = this.object.position.z+20*Math.sin(this.rotation);
+      this.sideRotation += this.deltaRotationX*0.02;
+      this.target.x = this.object.position.x+20*Math.cos(this.sideRotation);
+      this.target.z = this.object.position.z+20*Math.sin(this.sideRotation);
+    }
+
+    if (this.rotateUp) {
+      this.rotateUp = false;
+      this.upRotation += this.deltaRotationY*0.3;
+      this.target.y = this.object.position.y+this.upRotation;
+    }
+    if (this.rotateDown) {
+      this.rotateDown = false;
+      this.upRotation -= this.deltaRotationY*0.3;
+      this.target.y = this.object.position.y+this.upRotation;
     }
 
     this.object.lookAt(this.target);
@@ -110,6 +136,7 @@ var FirstPersonCamera = function () {
   // Придумать нормальную функцию вращения камеры
 
   var lastPageX = 0;
+  var lastPageY = 0;
 
   document.onmousemove = function(e) {
 
@@ -121,7 +148,17 @@ var FirstPersonCamera = function () {
       this.deltaRotationX = lastPageX - e.pageX;
       this.rotateLeft = true;
     }
+    if (e.pageY > lastPageY) {
+      this.deltaRotationY = e.pageY - lastPageY;
+      this.rotateDown = true;
+    }
+    if (e.pageY < lastPageY) {
+      this.deltaRotationY = lastPageY - e.pageY;
+      this.rotateUp = true;
+    }
+
     lastPageX = e.pageX;
+    lastPageY = e.pageY;
 
   }.bind(this);
 };
